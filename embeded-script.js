4efc6181f1664462f701cdbdf3d60f1ef9d5a6f6
aokabin <info.aokabin@gmail.com>
2018-07-14 21:06:32 +0900

    Initial commit

---
+++ embeded-script.js
@@ -0,0 +1,40 @@
+const main =  function() {
+  const host = location.hostname;
+  const path = location.pathname;
+
+  if (host === "twitter.com" && path === "/search") {
+    setInterval(repeat, 5000);
+    document.getElementsByClassName("Grid-cell")[0].style.display = "none";
+  }
+};
+
+const counter = function(str,seq){
+  return str.split(seq).length - 1;
+}
+
+const tweetNode = function(node) {
+  return node.parentNode.parentNode.parentNode.parentNode;
+}
+
+const removeSpam = function() {
+  [].forEach.call(document.getElementsByClassName("TweetTextSize"),function(e){
+    let text = e.innerText;
+    if (counter(text, "#") >= 3) {
+      tweetNode(e).style.display = "none";
+    }
+  });
+}
+
+const repeat = function(){
+  if (window.scrollY === 0) {
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("uiShortcutGotoTopOfScreen", true, true);
+    document.dispatchEvent(evt);
+  }
+  removeSpam();
+}
+
+window.onload = function () {
+  main();
+};
+
